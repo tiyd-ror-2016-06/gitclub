d8f5314f26deecd666ab547f7a353058a26172e9
James Dabbs <jamesdabbs@gmail.com>
2016-06-30 17:00:38 -0400

    Don't double-json on save

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -5,7 +5,7 @@ class AuthController < Devise::OmniauthCallbacksController
     user = User.where(github_id: data.uid).first_or_create! do |u|
       u.email       = data.info.email
       u.password    = SecureRandom.hex 64
-      u.github_data = data.to_h.to_json
+      u.github_data = data.to_h
     end
 
     sign_in user
